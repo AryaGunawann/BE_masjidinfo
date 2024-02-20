25dca4d64e84d22f2e7432bc86cdec635d51d8bf
AryaGunawan <aryadipanegarag@gmail.com>
2024-02-20 22:55:51 +0700

    fix

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const express = require("express");
 const app = express();
 const postsRoutes = require("./routes/posts.router");
 
-// Menggunakan rute-rute posts
 app.use("/api", postsRoutes);
 
 // Port yang digunakan untuk aplikasi
-const port = process.env.PORT || 3000; // Menggunakan nilai dari .env atau default 3000
+const port = process.env.PORT || 3000;
 
 // Mendengarkan pada port yang telah ditentukan
 app.listen(port, () => {
