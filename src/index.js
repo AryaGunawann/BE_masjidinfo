e880cc32fb8c94879c89aacb263b33b4f60f3d69
AryaGunawan <aryadipanegarag@gmail.com>
2024-09-09 21:47:46 +0700

    update cors blocked

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const historyRoutes = require("./routes/historyRoutes");
 
 app.use(
   cors({
-    origin: "https://masjidinfo-backend.vercel.app",
+    origin: ["http://localhost:3000", "https://masjidinfo-backend.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
   })
 );
