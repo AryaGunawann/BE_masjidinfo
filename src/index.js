72cca9dea1619ffe42d824d335a99767616742f1
AryaGunawan <aryadipanegarag@gmail.com>
2024-09-12 15:44:23 +0700

    + link cors

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:3000",
       "https://masjidinfo-backend.vercel.app",
       "https://masjidinfo.vercel.app",
+      "https://masjidinfo.my.id",
     ],
     methods: "GET,POST,PUT,DELETE",
   })
